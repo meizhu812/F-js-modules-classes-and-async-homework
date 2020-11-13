e7a60923437d9c59deef911d71704456b9b45f59
Zhu Mei <meizhu812@outlook.com>
2020-11-13 15:55:03 +0800

    completed practice3

--- src/__test__/practice3.test.js
+++ src/__test__/practice3.test.js
@@ -1,5 +1,5 @@
-import { getPoetry } from "../practice3";
 import fetchMock from "fetch-mock";
+import { getPoetryPromise, getPoetryAsync } from "../practice3";
 
 fetchMock.mock("https://v1.jinrishici.com/all.json", {
   content: "天清一雁远，海阔孤帆迟。",
@@ -8,7 +8,20 @@ fetchMock.mock("https://v1.jinrishici.com/all.json", {
   category: "古诗文-抒情-离别",
 });
 
-test("properties on kevin and winnie expected", async () => {
-  const result = await getPoetry();
-  expect(result).toEqual(['送张舍人之江东', '李白', '天清一雁远，海阔孤帆迟。']);
+test("get poetry using Promise", async () => {
+  const result = await getPoetryPromise();
+  expect(result).toEqual([
+    "送张舍人之江东",
+    "李白",
+    "天清一雁远，海阔孤帆迟。",
+  ]);
+});
+
+test("get poetry using async", async () => {
+  const result = await getPoetryAsync();
+  expect(result).toEqual([
+    "送张舍人之江东",
+    "李白",
+    "天清一雁远，海阔孤帆迟。",
+  ]);
 });
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,17 @@
-export const getPoetry
+const URL = "https://v1.jinrishici.com/all.json";
+const getPoetryPromise = async () => {
+  return fetch(URL)
+    .then((res) => res.json())
+    .then((data) => [data.origin, data.author, data.content])
+    .catch((reason) => new Error(reason));
+};
+const getPoetryAsync = async () => {
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    return [data.origin, data.author, data.content];
+  } catch (e) {
+    return e;
+  }
+};
+export { getPoetryPromise, getPoetryAsync };
